cae8c1441076987b46822dbbd96012503dfcf918
suzhen <suzhen@homeinpocket.net>
2019-11-13 12:46:32 +0800

    :checkered_flag: add flags

---
+++ consts.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+name = 'vcs-uploader'
+version = '1.0.0'
+author = 'suzhen'
+author_email = 'suzhen@homeinpocket.net'
+description = 'uploader for vcs, work with libupdate'
