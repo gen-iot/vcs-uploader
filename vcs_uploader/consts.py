da42883e0442e5892b648476348c061ad72610b3
suzhen <suzhen@homeinpocket.net>
2019-11-13 13:24:55 +0800

    :wheelchair: new version

--- vcs_uploader/consts.py
+++ vcs_uploader/consts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 name = 'vcs_uploader'
-version = '1.0.0'
+version = '1.0.1'
 author = 'suzhen'
 author_email = 'suzhen@homeinpocket.net'
 description = 'uploader for vcs, work with libupdate'
