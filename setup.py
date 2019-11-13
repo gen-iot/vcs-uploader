65e796fdba6f22d191e7d8ccbeef3b8004cff033
suzhen <suzhen@homeinpocket.net>
2019-11-13 13:24:29 +0800

    :bug: fix startup function path

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'vcsup = vcs_uploader.core.__main__:main '
+            'vcsup = vcs_uploader.__main__:main '
         ]
     }
 
